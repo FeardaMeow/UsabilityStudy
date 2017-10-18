dc19df165ba2856cffcc17e748ed2fa5e97907ba
feardameow <shwang1121@gmail.com>
2017-10-18 11:27:27 -0700

    Set up google sheets to write to log file

--- server.R
+++ server.R
@@ -11,6 +11,10 @@ library(shiny)
 library(shinydashboard)
 library(googlesheets)
 
+googlesheets::gs_auth(token = "shiny_app_token.rds")
+sheet_key <- "1VSSv36D8ngNDe9TAAtU0OLBQ2JoSiTFFleqa_Y3r6GA"
+ss <- googlesheets::gs_key(sheet_key)
+
 tabs.content <- list(list(Title = "Tab1", Content = "Tab1 content"),
                      list(Title = "Tab2", Content = "Tab2 content"),
                      list(Title = "Tab3", Content = "Tab3 content"))
