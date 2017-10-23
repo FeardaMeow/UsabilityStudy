9e49c6ba199c1d85acf406e2aebb2da8d4941b58
JundiLiu <jundiliu@uw.edu>
2017-10-23 16:53:31 -0700

    Add the login page

--- server.R
+++ server.R
@@ -6,11 +6,15 @@
 # 
 #    http://shiny.rstudio.com/
 #
-
+  
 library(shiny)
 library(shinydashboard)
 library(shinythemes)
 library(googlesheets)
+
+Logged = FALSE
+my_username <- "test"
+my_password <- "test"
 
 googlesheets::gs_auth(token = "shiny_app_token.rds")
 sheet_key <- "1VSSv36D8ngNDe9TAAtU0OLBQ2JoSiTFFleqa_Y3r6GA"
@@ -35,11 +39,54 @@ server <- function(input, output, session) {
 #    tabs <- lapply(1:length(tabs.content), function(i) tabPanel(tabs.content[[i]]$Title, tabs.content[[i]]$Content))
 #    do.call(tabBox, tabs)
 #  })
+  values <- reactiveValues(authenticated = FALSE)
   
+# Return the UI for a modal dialog with data selection input. If 'failed' 
+# is TRUE, then display a message that the previous value was invalid.
+  dataModal <- function(failed = FALSE) {
+    modalDialog(
+      textInput("username", "Username:"),
+      passwordInput("password", "Password:"),
+      footer = tagList(
+        # modalButton("Cancel"),
+        actionButton("ok", "OK")
+      )
+    )
+  }
+  
+# Show modal when button is clicked.  
+# This `observe` is suspended only whith right user credential
+  
+  obs1 <- observe({
+    showModal(dataModal())
+  })
+  
+# When OK button is pressed, attempt to authenticate. If successful,
+# remove the modal. 
+  
+  obs2 <- observe({
+    req(input$ok)
+    isolate({
+      Username <- input$username
+      Password <- input$password
+    })
+    Id.username <- which(my_username == Username)
+    Id.password <- which(my_password == Password)
+    if (length(Id.username) > 0 & length(Id.password) > 0) {
+      if (Id.username == Id.password) {
+        Logged <- TRUE
+        values$authenticated <- TRUE
+        obs1$suspend()
+        removeModal()
+        
+      } else {
+        values$authenticated <- FALSE
+      }     
+    }
+  })
   output$menuitem <- renderMenu({
     sidebarMenu(
       menu.content
     )
   })
 }
-
